612e4d9bcf5ca45be70b91a308f997044e64e80e
Le Anh Minh <18521098@gm.uit.edu.vn>
2024-01-24 09:45:33 +0700

    lung tung

--- src/routes/medias.routes.ts
+++ src/routes/medias.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 
 import { uploadImageController } from '~/controllers/medias.controller'
+import { accessTokenValidator, verifyEmailValidator } from '~/middlewares/users.middleware'
 import { requestHandler } from '~/utils/support'
 const mediasRouter: Router = Router()
 
-mediasRouter.post('/upload-image', requestHandler(uploadImageController))
+mediasRouter.post('/upload-image', accessTokenValidator, verifyEmailValidator, requestHandler(uploadImageController))
 
 export default mediasRouter
